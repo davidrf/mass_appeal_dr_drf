65026f616d13b286a2458e8e6f82fe495081ae21
David Rodriguez Fuentes <David@Davids-MacBook-Pro.local>
2017-04-03 12:58:24 -0400

    create user

---
+++ web/models/user.ex
@@ -0,0 +1,34 @@
+defmodule MassAppeal.User do
+  use MassAppeal.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, [:email, :name])
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 7)
+    |> put_pass_hash
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ -> changeset
+    end
+  end
+end
